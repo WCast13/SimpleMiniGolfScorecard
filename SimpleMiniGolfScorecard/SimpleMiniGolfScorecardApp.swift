39b203d71e7c2491b50ad2b99be1f108e9afc511
William Castellano <wcastellano13@gmail.com>
2025-11-13 11:18:36 -0500

    Initial Commit

---
+++ SimpleMiniGolfScorecard/SimpleMiniGolfScorecardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMiniGolfScorecardApp.swift
+//  SimpleMiniGolfScorecard
+//
+//  Created by William Castellano on 11/13/25.
+//
+
+import SwiftUI
+
+@main
+struct SimpleMiniGolfScorecardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
